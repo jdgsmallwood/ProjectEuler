b5deeb37af2f673397f2ec48e1adb6c7b65deb51
justin <justin.d.smallwood@gmail.com>
2017-12-02 22:51:36 +0100

    Problem 11 fixed

--- Problem 11.py
+++ Problem 11.py
@@ -8,48 +8,57 @@ for i in range(len(probleminputlines)):
     data[i] = numbers
 
 
-largest = 0
+largesth = 0
+largestv = 0
+largestdtl = 0
+largestdbl = 0
 n = 4
 #Horizontal
 for i in range(len(probleminputlines)):
-    for j in range(len(data[i])-n):
+    for j in range(len(data[i])-n+1):
         prod = 1
         for k in range(n):
             prod *= data[i][j+k]
-        if prod > largest:
-            largest = prod
+        if prod > largesth:
+            largesth = prod
 
 
 #Vertical
 for j in range(len(probleminputlines)):
-    for i in range(len(data[i])-n):
+    for i in range(len(data[i])-n+1):
         prod = 1
         for k in range(n):
             prod*=data[i+k][j]
-        if prod > largest:
-            largest = prod
+        if prod > largestv:
+            largestv = prod
 
 
 #Diagonal Top Left - Bottom Right
 for i in range(len(probleminputlines)-n):
-    for j in range(len([probleminputlines]) - n):
+    for j in range(len(probleminputlines) - n+1):
         prod = 1
         for k in range(n):
             prod *= data[i+k][j+k]
-        if prod > largest:
-            largest = prod
+        if prod > largestdtl:
+            largestdtl = prod
 
 
 #Diagonal Top Right - Bottom Left
-for j in range(len(probleminputlines)-n):
-    for i in range(n-1,len([probleminputlines])):
+for j in range(len(probleminputlines)-n+1):
+    for i in range(n-1,len(probleminputlines)):
         prod = 1
         for k in range(n):
-            prod *= data[i-k, j+k]
-        if prod > largest:
-            largest = prod
+            prod *= data[i-k][j+k]
+        if prod > largestdbl:
+            largestdbl = prod
 
-print(largest)
+print(largestv)
+print(largesth)
+print(largestdtl)
+print(largestdbl)
+
+largest = max(largestv, largesth, largestdbl, largestdtl)
+print("largest is %i" % largest)
 
 
 
